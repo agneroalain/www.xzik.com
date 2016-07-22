e437cf17803819360acf7c784ae8b45086c969d9
AgneroAlain <agneroalainphoto@gmail.com>
2016-07-22 10:17:01 +0000

    Creation des capsules sur musique.php

--- musique.php
+++ musique.php
@@ -7,11 +7,54 @@
         <div class="row">
             <div class="col">
                 <div class="musique">
-                    <i class="fa fa-play-circle fa-2x bt_play"> </i>
+                    <i class="fa fa-play-circle fa-1x bt bt_play"> </i>
+                    <i class="fa fa-download fa-1x bt bt_download"> </i>
+                    <i class="fa fa-eye fa-1x bt bt_voir"> </i>
                     <div class="musique_cover"></div>
-                    <span class="musique_nom">Titre de la musique</span>
-                    <div class="artiste_musique"><span class="musique_artiste">Artiste</span> <div class="thumb_artiste"></div></div>
-                    <span class="musique_duree">03 : 04</span>
+                    <div>
+                        <span class="musique_nom">Titre de la musique</span>
+                        <div class="artiste_musique"><span class="musique_artiste">Artiste</span> <div class="thumb_artiste"></div></div>
+                        <span class="musique_duree">03 : 04</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="musique">
+                    <i class="fa fa-play-circle fa-1x bt bt_play"> </i>
+                    <i class="fa fa-download fa-1x bt bt_download"> </i>
+                    <i class="fa fa-eye fa-1x bt bt_voir"> </i>
+                    <div class="musique_cover"></div>
+                    <div>
+                        <span class="musique_nom">Titre de la musique</span>
+                        <div class="artiste_musique"><span class="musique_artiste">Artiste</span> <div class="thumb_artiste"></div></div>
+                        <span class="musique_duree">03 : 04</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="musique">
+                    <i class="fa fa-play-circle fa-1x bt bt_play"> </i>
+                    <i class="fa fa-download fa-1x bt bt_download"> </i>
+                    <i class="fa fa-eye fa-1x bt bt_voir"> </i>
+                    <div class="musique_cover"></div>
+                    <div>
+                        <span class="musique_nom">Titre de la musique</span>
+                        <div class="artiste_musique"><span class="musique_artiste">Artiste</span> <div class="thumb_artiste"></div></div>
+                        <span class="musique_duree">03 : 04</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="musique">
+                    <i class="fa fa-play-circle fa-1x bt bt_play"> </i>
+                    <i class="fa fa-download fa-1x bt bt_download"> </i>
+                    <i class="fa fa-eye fa-1x bt bt_voir"> </i>
+                    <div class="musique_cover"></div>
+                    <div>
+                        <span class="musique_nom">Titre de la musique</span>
+                        <div class="artiste_musique"><span class="musique_artiste">Artiste</span> <div class="thumb_artiste"></div></div>
+                        <span class="musique_duree">03 : 04</span>
+                    </div>
                 </div>
             </div>
             
